250e59f36f13122f96b58a5b10f3f04ce9908d16
packocrayons <gibson.brydon@gmail.com>
2017-11-20 11:07:10 -0500

    Cleaned up some things, converted to python3 since I needed that
    Added real logging :
    
    	Logger logs to /var/log/udpSender.py (This requires sudo, I figured that's fine since most of the ports we're using are root only)
    	This logger format was just as much a proof of concept so that it can be used in future larger applications
    
    Added a verifyAck function :
    
    	At the moment, it only returns true, but can be used to check for valid ack format in the future (format must be decided on)

--- udpsender.py
+++ udpsender.py
@@ -1,26 +1,38 @@
 import socket
 import sys
+import logging
+import logging.handlers
 
 
-SOCKET_TIMEOUT = 2 #socket timeout waiting for ack in seconds
+SOCKET_TIMEOUT = 5 #socket timeout waiting for ack in seconds
 SOCKET_RCV_BUFSIZE = 100 #socket buffer size for ack packet
 
 
+def verifyAck(ackMsg):
+	if (True):
+		logger.info('Received a valid ack')
+		return True
+	else:
+		logger.warning('Received an invalid ack')
+		return False
+
+
 def sendPacketUntilAck(ipv6Address, port, msg):
-	successFlag = False
 	sock.bind(('0.0.0.0', port))
-	while (successFlag == False):
+	while (True):
 		sock.sendto(msg, (ipv6Address, port)) #send the packet 
+		logmsg = 'Sending packet : ' + msg.decode()
+		logger.info(logmsg)
 		try:
 			ackMsg, incomingaddr = sock.recvfrom(SOCKET_RCV_BUFSIZE)
-			if (True): #if message is a valid ack - assume it is for now
-				successFlag = True
+			if (verifyAck(ackMsg)): #if message is a valid ack - verifyack takes care of logging
+				break
 		except socket.timeout:
-			print 'socket timed out, retransmitting' #do nothing and let the while loop take care of retransmit
+			logger.warning('Socket timed out, retransmitting') #do nothing and let the while loop take care of retransmit
 
 
 if len(sys.argv) < 3: #[self], ip address and port
-	print 'Usage: python udpsender.py IP_ADDRESS PORT'
+	print('Usage: python udpsender.py IP_ADDRESS PORT')
 	sys.exit(0)
 
 #sys.argv is at least 2 long
@@ -30,5 +42,15 @@ outgoingPort = sys.argv[2]
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.settimeout(SOCKET_TIMEOUT)
 
-msg = 'Hello internet'
-sendPacketUntilAck(outgoingIp, int(outgoingPort), msg)
+logger = logging.getLogger('updSenderLogger')
+hdlr = logging.handlers.RotatingFileHandler('/var/log/udpSender.log')
+hdlr.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s]: %(message)s'))
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
+logger.info('Starting up')
+#logger.setLevel() - don't do this for now, we want to log everything
+
+
+while(True):
+	msg = input(">> ")
+	sendPacketUntilAck(outgoingIp, int(outgoingPort), msg.encode())
