771c9526fc342a335ed8cc31db4d13ee35fba4ef
Brendan Lucas <brendanwflucas@gmail.com>
2017-11-29 23:52:44 -0500

    convert file to adhere to PEP8 standards

--- udpsender.py
+++ udpsender.py
@@ -4,38 +4,38 @@ import logging
 import logging.handlers
 
 
-SOCKET_TIMEOUT = 5 #socket timeout waiting for ack in seconds
-SOCKET_RCV_BUFSIZE = 100 #socket buffer size for ack packet
+SOCKET_TIMEOUT = 5   # socket timeout waiting for ack in seconds
+SOCKET_RCV_BUFSIZE = 100   # socket buffer size for ack packet
 
 
 def verifyAck(ackMsg):
-	if (True):
-		logger.info('Received a valid ack')
-		return True
-	else:
-		logger.warning('Received an invalid ack')
-		return False
+    if (True):
+        logger.info('Received a valid ack')
+        return True
+    else:
+        logger.warning('Received an invalid ack')
+        return False
 
 
 def sendPacketUntilAck(ipv6Address, port, msg):
-	sock.bind(('0.0.0.0', port))
-	while (True):
-		sock.sendto(msg, (ipv6Address, port)) #send the packet 
-		logmsg = 'Sending packet : ' + msg.decode()
-		logger.info(logmsg)
-		try:
-			ackMsg, incomingaddr = sock.recvfrom(SOCKET_RCV_BUFSIZE)
-			if (verifyAck(ackMsg)): #if message is a valid ack - verifyack takes care of logging
-				break
-		except socket.timeout:
-			logger.warning('Socket timed out, retransmitting') #do nothing and let the while loop take care of retransmit
+    sock.bind(('0.0.0.0', port))
+    while (True):
+        sock.sendto(msg, (ipv6Address, port))  # send the packet
+        logmsg = 'Sending packet : ' + msg.decode()
+        logger.info(logmsg)
+        try:
+            ackMsg, incomingaddr = sock.recvfrom(SOCKET_RCV_BUFSIZE)
+            if (verifyAck(ackMsg)):  # if message is a valid ack - verifyack takes care of logging
+                break
+        except socket.timeout:
+            logger.warning('Socket timed out, retransmitting')  # do nothing and let the while loop take care of retransmit
 
 
-if len(sys.argv) < 3: #[self], ip address and port
-	print('Usage: python udpsender.py IP_ADDRESS PORT')
-	sys.exit(0)
+if len(sys.argv) < 3:  # [self], ip address and port
+    print('Usage: python udpsender.py IP_ADDRESS PORT')
+    sys.exit(0)
 
-#sys.argv is at least 2 long
+# sys.argv is at least 2 long
 outgoingIp = sys.argv[1]
 outgoingPort = sys.argv[2]
 
@@ -48,9 +48,9 @@ hdlr.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s]: %(message)s'))
 logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 logger.info('Starting up')
-#logger.setLevel() - don't do this for now, we want to log everything
+# logger.setLevel() - don't do this for now, we want to log everything
 
 
 while(True):
-	msg = input(">> ")
-	sendPacketUntilAck(outgoingIp, int(outgoingPort), msg.encode())
+    msg = input(">> ")
+    sendPacketUntilAck(outgoingIp, int(outgoingPort), msg.encode())
