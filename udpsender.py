8749369ae96e5c52b53efb983987fc9718ac72fe
packocrayons <gibson.brydon@gmail.com>
2017-12-08 10:21:12 -0500

    First attempt at ipv6 with lowpan0

--- udpsender.py
+++ udpsender.py
@@ -9,31 +9,31 @@ SOCKET_RCV_BUFSIZE = 100   # socket buffer size for ack packet
 
 
 def verifyAck(ackMsg):
-    if (True):
-        logger.info('Received a valid ack')
-        return True
-    else:
-        logger.warning('Received an invalid ack')
-        return False
+	if (True):
+	    logger.info('Received a valid ack')
+	    return True
+	else:
+	    logger.warning('Received an invalid ack')
+	    return False
 
 
 def sendPacketUntilAck(ipv6Address, port, msg):
-    sock.bind(('0.0.0.0', port))
-    while (True):
-        sock.sendto(msg, (ipv6Address, port))  # send the packet
-        logmsg = 'Sending packet : ' + msg.decode()
-        logger.info(logmsg)
-        try:
-            ackMsg, incomingaddr = sock.recvfrom(SOCKET_RCV_BUFSIZE)
-            if (verifyAck(ackMsg)):  # if message is a valid ack - verifyack takes care of logging
-                break
-        except socket.timeout:
-            logger.warning('Socket timed out, retransmitting')  # do nothing and let the while loop take care of retransmit
+	sock.bind(('::', port, 0, socket.if_nametoindex('lowpan0')));
+	while (True):
+		sock.sendto(msg, (ipv6Address, port))  # send the packet
+		logmsg = 'Sending packet : ' + msg.decode()
+		logger.info(logmsg)
+		try:
+			ackMsg, incomingaddr = sock.recvfrom(SOCKET_RCV_BUFSIZE)
+			if (verifyAck(ackMsg)):  # if message is a valid ack - verifyack takes care of logging
+				break
+		except socket.timeout:
+			logger.warning('Socket timed out, retransmitting')  # do nothing and let the while loop take care of retransmit
 
 
 if len(sys.argv) < 3:  # [self], ip address and port
-    print('Usage: python udpsender.py IP_ADDRESS PORT')
-    sys.exit(0)
+	print('Usage: python udpsender.py IP_ADDRESS PORT')
+	sys.exit(0)
 
 # sys.argv is at least 2 long
 outgoingIp = sys.argv[1]
@@ -52,5 +52,5 @@ logger.info('Starting up')
 
 
 while(True):
-    msg = input(">> ")
-    sendPacketUntilAck(outgoingIp, int(outgoingPort), msg.encode())
+	msg = input(">> ")
+	sendPacketUntilAck(outgoingIp, int(outgoingPort), msg.encode())
