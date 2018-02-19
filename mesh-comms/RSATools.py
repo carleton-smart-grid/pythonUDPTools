32d4e0ab30d15e61429aae77f53c39e130426746
argRobertson <a.r.g.robertson@gmail.com>
2018-02-19 02:40:39 -0500

    Add files via upload
    Like last time, but this time I actually included the file

---
+++ mesh-comms/RSATools.py
@@ -0,0 +1,48 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+#This is all a very basic implementation of RSA,
+#Further work can be done to make it better
+#Namely signing with the private key of the sender to authenticate it.
+#Also should add passwords to the key file, but fuck that for now.
+
+#Generate a pair of public and private keys saving them in the appropriate file
+def generateRSAKeys ():
+    key = RSA.generate(2048)
+    f = open('key.bin', 'wb')
+    fpub = open('publickey.bin', 'wb')
+    fpub.write(key.publickey().exportKey('PEM'))
+    f.write(key.exportKey('PEM'))
+    f.close()
+    fpub.close()
+    return
+
+#Encrypt passed dat currently only encrpyting with the public key
+#private key is not currently used to authenticate data
+def encryptRSA( dat):
+
+    keyString = open('publickey.bin', 'rb').read()
+    key = RSA.import_key(keyString)
+    cipher = PKCS1_OAEP.new(key)
+    encryptedData = cipher.encrypt(dat)
+    return encryptedData
+
+#Takes the data encrypted with the public key and decrypts it with the private keys
+#Public key authentication is not currently written
+def decryptRSA( dat):
+
+    keyString = open('key.bin', 'rb').read()
+    key = RSA.importKey(keyString)
+    cipher = PKCS1_OAEP.new(key)
+    unencryptedData = cipher.decrypt(dat)
+    return unencryptedData
+
+#must have already generated keys to use the test.
+def simpleTestRSA():
+    data = b'Please end my suffering'
+    encryptedData = encryptRSA(data)
+    decryptedData = decryptRSA(encryptedData)
+    if(data == decryptedData):
+        print ('It works')
+    else:
+        print ('it is fucked')
